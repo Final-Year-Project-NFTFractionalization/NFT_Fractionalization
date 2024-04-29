2310a41b27107af3df0726a8e3624c8441309f5e
Saad Khan <saad>
2024-04-29 15:18:53 +0500

    added validation and rendered

--- src/components/PropertyListingform.js
+++ src/components/PropertyListingform.js
@@ -13,6 +13,7 @@ const PropertyListingform = () => {
     sqft: '',
     address: '',
   });
+  const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
     const { name, value, type } = e.target;
@@ -32,30 +33,62 @@ const PropertyListingform = () => {
     }
   };
 
+  const validateForm = () => {
+    const errors = {};
+    if (!listing.name.trim()) {
+      errors.name = 'Name is required';
+    }
+    if (!listing.description.trim()) {
+      errors.description = 'Description is required';
+    }
+    if (!listing.image) {
+      errors.image = 'Image is required';
+    }
+    if (!listing.price.trim() || isNaN(listing.price)) {
+      errors.price = 'Price must be a number';
+    }
+    if (!listing.beds.trim() || isNaN(listing.beds)) {
+      errors.beds = 'Beds must be a number';
+    }
+    if (!listing.bath.trim() || isNaN(listing.bath)) {
+      errors.bath = 'Bath must be a number';
+    }
+    if (!listing.sqft.trim() || isNaN(listing.sqft)) {
+      errors.sqft = 'Square Feet must be a number';
+    }
+    if (!listing.address.trim()) {
+      errors.address = 'Address is required';
+    }
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
-    try{
-      // Make a request to the Node.js script to add data to IPFS
-      const response = await axios.post('http://localhost:3002/addDataToIPFS', listing);
 
-      //extract cid from response
-      const cid = response.data.cid;
-      
-      // Handle the CID as needed (e.g., store it in a database, display it to the user)
-       console.log('IPFS CID:', cid);
+    if (validateForm()) {
+      try {
+        // Make a request to the Node.js script to add data to IPFS
+        const response = await axios.post('http://localhost:3002/addDataToIPFS', listing);
 
-      // Log the form data
-      console.log('Submitted Form Data:', listing);
-    } catch (error) {
-      console.log('Error adding property to IPFS', error);
+        // Extract cid from response
+        const cid = response.data.cid;
+
+        // Handle the CID as needed (e.g., store it in a database, display it to the user)
+        console.log('IPFS CID:', cid);
+
+        // Log the form data
+        console.log('Submitted Form Data:', listing);
+      } catch (error) {
+        console.log('Error adding property to IPFS', error);
+      }
     }
   };
 
   return (
     <>
       <header className='heading'>
-        <div class="background-containers"></div>
+        <div className="background-containers"></div>
         <h1 className='header__titlee'>Sell your home with confidence</h1>
       </header>
 
@@ -100,35 +133,43 @@ const PropertyListingform = () => {
               <label>
                 Name
                 <input type="text" name="name" className="inputs" value={listing.name} onChange={handleChange} />
+                {errors.name && <div className="error">{errors.name}</div>}
               </label>
               <label>
                 Description
                 <textarea name="description" className="inputs" value={listing.description} onChange={handleChange} />
+                {errors.description && <div className="error">{errors.description}</div>}
               </label>
               <label>
                 House Picture
                 <br/><br/>
                 <input type="file" name="image" accept="image/png" onChange={handleChange} />
+                {errors.image && <div className="error">{errors.image}</div>}
               </label><br/>
               <label>
                 Purchase Price (ETH)
                 <input type="text" name="price" className="inputs" value={listing.price} onChange={handleChange} />
+                {errors.price && <div className="error">{errors.price}</div>}
               </label>
               <label>
                 Bed Rooms
                 <input type="text" name="beds" className="inputs" value={listing.beds} onChange={handleChange} />
+                {errors.beds && <div className="error">{errors.beds}</div>}
               </label>
               <label>
                 Bathrooms
                 <input type="text" name="bath" className="inputs" value={listing.bath} onChange={handleChange} />
+                {errors.bath && <div className="error">{errors.bath}</div>}
               </label>
               <label>
                 Square Feet
                 <input type="text" name="sqft" className="inputs" value={listing.sqft} onChange={handleChange} />
+                {errors.sqft && <div className="error">{errors.sqft}</div>}
               </label>
               <label>
                 Address
                 <input type="text" name="address" className="inputs" value={listing.address} onChange={handleChange} />
+                {errors.address && <div className="error">{errors.address}</div>}
               </label>
               <button type="submit"><b>List Property</b></button>
             </form>
