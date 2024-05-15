ccab6e6e3daae6a2bc7dd04811022f843aeb5a37
Munir Abbasi <87448868+Munir-2001@users.noreply.github.com>
2024-05-15 13:37:37 +0500

    NEVER GIVE UP

--- src/App.js
+++ src/App.js
@@ -85,22 +85,27 @@ const loadBlockchainData = async()=>{
         <hr />
 
         <div className='cards'>
-          {homes.map((home, index) => (
-            <div className='card' key={index} onClick={() => togglePop(home)}>
-              <div className='card__image'>
-                <img src={home.image} alt="Home" />
-              </div>
-              <div className='card__info'>
-                <h4>{home.attributes[0].value} ETH</h4>
-                <p>
-                  <strong>{home.attributes[2].value}</strong> bds |
-                  <strong>{home.attributes[3].value}</strong> ba |
-                  <strong>{home.attributes[4].value}</strong> sqft
-                </p>
-                <p>{home.address}</p>
-              </div>
-            </div>
-          ))}
+        {homes.map((home, index) => (
+  <div className='card' key={index} onClick={() => togglePop(home)}>
+    <div className='card__image'>
+      {home.image ? (
+        <img src={home.image} alt="Home" />
+      ) : (
+        <img src={`http://127.0.0.1:8080/ipfs/${home.imageCID}`} alt="Home" />
+      )}
+    </div>
+    <div className='card__info'>
+      <h4>{home.attributes ? (Array.isArray(home.attributes) ? home.attributes[0].value : home.attributes.price) : ''} ETH</h4>
+      <p>
+        <strong>{home.attributes ? (Array.isArray(home.attributes) ? home.attributes[2].value : home.attributes.beds) : ''}</strong> bds |
+        <strong>{home.attributes ? (Array.isArray(home.attributes) ? home.attributes[3].value : home.attributes.bath) : ''}</strong> ba |
+        <strong>{home.attributes ? (Array.isArray(home.attributes) ? home.attributes[4].value : home.attributes.sqft) : ''}</strong> sqft
+      </p>
+      <p>{home.address}</p>
+    </div>
+  </div>
+))}
+
         </div>
       </div></div>}></Route>
       <Route path="/PropertyListingform" element={<PropertyListingform />}></Route>
